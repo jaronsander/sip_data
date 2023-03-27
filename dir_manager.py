f5d8596e9b5ece5edba27114bc377c086e9a781a
jaronsander <jar.sand.on@gmail.com>
2023-03-26 18:41:00 -0700

    changed files 2

--- dir_manager.py
+++ dir_manager.py
@@ -33,9 +33,16 @@ def annotate_files(dir_list):
                 label = int(f.split('_')[2].split('.')[0])
                 df.append([f, root, label])
     annotations = pd.DataFrame(df, columns=["file", "folder", "label"])
-    annotations.to_csv("p_fall_annotations.csv")
+    annotations.to_csv("fall_annotations.csv")
 
 if __name__ == "__main__":
-    annotate_files(['percussive_audio/ambient noise',
-                    'percussive_audio/water_falls', 'percussive_audio/tap_falls', 'percussive_audio/talk_falls',
-                    'percussive_audio/shower_falls','percussive_audio/random_falls', 'percussive_audio/quiet_falls', 'percussive_audio/fan_falls', 'percussive_audio/drip_falls', 'percussive_audio/chair_falls','percussive_audio/ambient_falls'])
+    # annotate_files(['percussive_audio/ambient noise',
+    #                 'percussive_audio/water_falls', 'percussive_audio/tap_falls', 'percussive_audio/talk_falls',
+    #                 'percussive_audio/shower_falls','percussive_audio/random_falls', 'percussive_audio/quiet_falls', 'percussive_audio/fan_falls', 'percussive_audio/drip_falls', 'percussive_audio/chair_falls','percussive_audio/ambient_falls'])
+    annotate_files(['ambient noise',
+                    'water_falls', 'tap_falls',
+                    'talk_falls',
+                    'shower_falls', 'random_falls',
+                    'quiet_falls', 'fan_falls',
+                    'drip_falls', 'chair_falls',
+                    'ambient_falls'])
